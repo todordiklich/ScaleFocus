72430ad906ff99fc341f54a8ee12d6c1207b83d4
todorzv <todorzv@abv.bg>
2021-07-01 17:04:56 +0300

    Add Base Models template

---
+++ Final Project/DeletableAndAuditModels/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNetCoreTemplate.Data.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class BaseModel : IAuditInfo
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
